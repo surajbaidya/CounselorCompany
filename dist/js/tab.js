c6d7043734dcf9b59244dfc85c36a354f033d609
Suraj Baidya <suraj@bentraytech.com>
2024-05-13 10:20:15 +0545

    design tab section

--- dist/js/tab.js
+++ dist/js/tab.js
@@ -4,7 +4,7 @@ function home_display() {
   document.getElementById("code-txt").style.display = "none";
   document.getElementById("project-txt").style.display = "none";
   document.getElementById("about-txt").style.display = "none";
-  document.getElementById("personal-txt").style.display = "none";
+  document.getElementById("business-txt").style.display = "none";
 
   document.getElementById("home-lb").style.background = "#589167";
   document.getElementById("home-lb").style.borderRadius = "5px";
@@ -12,7 +12,7 @@ function home_display() {
   document.getElementById("code-lb").style.background = "none";
   document.getElementById("project-lb").style.background = "none";
   document.getElementById("about-lb").style.background = "none";
-  document.getElementById("personal-lb").style.background = "none";
+  document.getElementById("business-lb").style.background = "none";
 }
 
 function blog_display() {
@@ -21,7 +21,7 @@ function blog_display() {
   document.getElementById("code-txt").style.display = "none";
   document.getElementById("project-txt").style.display = "none";
   document.getElementById("about-txt").style.display = "none";
-  document.getElementById("personal-txt").style.display = "none";
+  document.getElementById("business-txt").style.display = "none";
 
   document.getElementById("home-lb").style.background = "none";
   document.getElementById("blog-lb").style.background = "#589167";
@@ -29,7 +29,7 @@ function blog_display() {
   document.getElementById("code-lb").style.background = "none";
   document.getElementById("project-lb").style.background = "none";
   document.getElementById("about-lb").style.background = "none";
-  document.getElementById("personal-lb").style.background = "none";
+  document.getElementById("business-lb").style.background = "none";
 }
 
 function code_display() {
@@ -38,14 +38,14 @@ function code_display() {
   document.getElementById("code-txt").style.display = "block";
   document.getElementById("project-txt").style.display = "none";
   document.getElementById("about-txt").style.display = "none";
-  document.getElementById("personal-txt").style.display = "none";
+  document.getElementById("business-txt").style.display = "none";
 
   document.getElementById("home-lb").style.background = "none";
   document.getElementById("blog-lb").style.background = "none";
   document.getElementById("code-lb").style.background = "#589167";
   document.getElementById("project-lb").style.background = "none";
   document.getElementById("about-lb").style.background = "none";
-  document.getElementById("personal-lb").style.background = "none";
+  document.getElementById("business-lb").style.background = "none";
 }
 
 function project_display() {
@@ -54,14 +54,14 @@ function project_display() {
   document.getElementById("code-txt").style.display = "none";
   document.getElementById("project-txt").style.display = "block";
   document.getElementById("about-txt").style.display = "none";
-  document.getElementById("personal-txt").style.display = "none";
+  document.getElementById("business-txt").style.display = "none";
 
   document.getElementById("home-lb").style.background = "none";
   document.getElementById("blog-lb").style.background = "none";
   document.getElementById("code-lb").style.background = "none";
   document.getElementById("project-lb").style.background = "#589167";
   document.getElementById("about-lb").style.background = "none";
-  document.getElementById("personal-lb").style.background = "none";
+  document.getElementById("business-lb").style.background = "none";
 }
 
 function about_display() {
@@ -70,28 +70,28 @@ function about_display() {
   document.getElementById("code-txt").style.display = "none";
   document.getElementById("project-txt").style.display = "none";
   document.getElementById("about-txt").style.display = "block";
-  document.getElementById("personal-txt").style.display = "none";
+  document.getElementById("business-txt").style.display = "none";
 
   document.getElementById("home-lb").style.background = "none";
   document.getElementById("blog-lb").style.background = "none";
   document.getElementById("code-lb").style.background = "none";
   document.getElementById("project-lb").style.background = "none";
   document.getElementById("about-lb").style.background = "#589167";
-  document.getElementById("personal-lb").style.background = "none";
+  document.getElementById("business-lb").style.background = "none";
 }
 
-function personal_display() {
+function business_display() {
   document.getElementById("home-txt").style.display = "none";
   document.getElementById("blog-txt").style.display = "none";
   document.getElementById("code-txt").style.display = "none";
   document.getElementById("project-txt").style.display = "none";
   document.getElementById("about-txt").style.display = "none";
-  document.getElementById("personal-txt").style.display = "block";
+  document.getElementById("business-txt").style.display = "block";
 
   document.getElementById("home-lb").style.background = "none";
   document.getElementById("blog-lb").style.background = "none";
   document.getElementById("code-lb").style.background = "none";
   document.getElementById("project-lb").style.background = "none";
   document.getElementById("about-lb").style.background = "none";
-  document.getElementById("personal-lb").style.background = "#589167";
+  document.getElementById("business-lb").style.background = "#589167";
 }
